e3ec53ec2469b1a8b585655d73f9ae6e3ea92e1f
pahu <paulhuts@gmail.com>
2019-04-14 19:37:07 +1200

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from sensors import DS18B20
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def webapp():
-    s = "<h1>Welcome to Toroi Pi</h1>"
-    s += "<p>temperature sensor data</p>"
-    id = '28-041752558aff'
-    temperature = DS18B20.get_temperature(id)
-    s = s + "id: " + id + "<br>"
-    s = s + "temperature: " + str(temperature)
-    return s
+def home():    
+    sensorId = '28-041752558aff'
+    temperature = DS18B20.get_temperature(sensorId)
+    return render_template('home.html', sensorId=sensorId, temperature=temperature)
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
+if __name__ == '__main__':
+    app.run(debug=True, port=6100)
